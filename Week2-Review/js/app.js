157ecf3e16a067ee82c8157c5f22bfdede392665
Drew Harbaugh <dharbaugh2290@gmail.com>
2020-11-23 14:17:54 -0500

    Pair final

--- Week2-Review/js/app.js
+++ Week2-Review/js/app.js
@@ -1,8 +1,11 @@
+//@ts-nocheck
+
 const NUMBER_OF_PROBLEMS = 10;
 let correctAnswers = 0;
 let randomOne = getRandomNumber(10);
 let randomTwo = getRandomNumber(10);
 let correctAnswer = randomOne * randomTwo;
+let questionNumber = 1;
 
 
 document.addEventListener('DOMContentLoaded', e => {
@@ -33,54 +36,89 @@ let array = [correctAnswer, getRandomNumber(100), getRandomNumber(100), getRando
 let choices = shuffleArray(array);
 
 
+
 function startQuiz() {
-    const expression = document.getElementById('problem');
+    const expression = document.querySelector('#problem div');
     expression.innerText = randomOne + ' * ' + randomTwo;
+
+    const tracker = document.querySelector('#problem p');
+    tracker.innerText = 'Problem: ' + questionNumber + '/' + NUMBER_OF_PROBLEMS + ' | Score: ' + correctAnswers;
+
 }
 
 
+
+
 function displayAnswers() {
+    document.getElementById('answers').style.display = '';
     let display = document.getElementById('answers');
     const ul = document.querySelector('ul');
     let array = [correctAnswer, getRandomNumber(100), getRandomNumber(100), getRandomNumber(100)];
     let choices = shuffleArray(array);
-    
-
     array.forEach(choices => {
-        const li = document.querySelector('li');
-        li.innerText = choices;
-        ul.appendChild(li);
+        const listItem = document.querySelector('li');
+        listItem.innerText = choices;
+        ul.appendChild(listItem);
     });
 }
 
 function nextQuestion() {
-    const expression = document.getElementById('problem');
+    const expression = document.querySelector('#problem div');
     randomOne = getRandomNumber(10);
     randomTwo = getRandomNumber(10);
     correctAnswer = randomOne * randomTwo;
     expression.innerText = randomOne + ' * ' + randomTwo;
+    const tracker = document.querySelector('#problem p');
+    tracker.innerText = 'Problem: ' + questionNumber + '/' + NUMBER_OF_PROBLEMS + ' | Score: ' + correctAnswers;
+
 
 }
 
 
 function submitAnswer(event) {
+    questionNumber++;
     console.log(event.target.innerText);
     let selectedAnswer = event.target.innerText;
+    
     if (selectedAnswer === String(correctAnswer)) {
         console.log('correct');
         correctAnswers++;
     }
+    
+    if (questionNumber === NUMBER_OF_PROBLEMS + 1) {
+        endScreen();
+
+    } else {
+        nextQuestion();
+        displayAnswers();
+    }
+    
+
+}
+
+function endScreen() {
+    document.getElementById('answers').style.display = 'none';
+    
+    const hide = document.querySelector('.show-hide');
+    hide.innerText = '';
+
+
+    const expression = document.querySelector('#problem div');
+    expression.innerText = '';
+
+    // const tracker = document.querySelector('ul');
+    // tracker.innerText = '';
+
+    // document.getElementsByClassName('show-hide')[0].style.visibilty = 'hidden';
+}
+
+function startOver(event) {
+    correctAnswers = 0;
+    questionNumber = 1;
+    startQuiz();
     nextQuestion();
     displayAnswers();
 
-}
-
-
-function startOver() {
-}
-
-
-function displayScore() {
 }
 
 
